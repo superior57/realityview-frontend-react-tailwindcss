66f9bf9a09ce6e05ad64927cf8c7590286678e5e
Mihai <mihai@realtyview.com>
2023-08-23 23:34:51 +0300

    feat: add adornment component

---
+++ src/components/baseComponents/Adornment/index.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import classNames from "classnames";
+// types
+import { InputAdornmentType } from "./types";
+
+// ----------------------------------------------------------------------
+type Props = {
+  className?: string;
+  type?: InputAdornmentType;
+  children: ReactNode;
+};
+
+const InputAdornment = ({ className, type = "inherit", children }: Props) => {
+  return (
+    <div
+      className={classNames(
+        "absolute inset-y-0 flex items-center pointer-events-none",
+        className,
+        {
+          ["bg-secondary px-8 rounded-5"]: type === "fill",
+        }
+      )}
+    >
+      <div
+        className={classNames("h-24 flex items-center justify-center", {
+          ["w-24"]: type === "inherit",
+        })}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default InputAdornment;
---
+++ src/components/baseComponents/Adornment/types.tsx
@@ -0,0 +1,1 @@
+export type InputAdornmentType = "inherit" | "fill";
