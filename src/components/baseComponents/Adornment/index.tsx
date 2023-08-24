31028e1550e863e03f15fd1b919c6668c855ebf1
Mihai <mihai@realtyview.com>
2023-08-24 18:12:14 +0300

    fix: resolve cursor will be work on Adornment component

--- src/components/baseComponents/Adornment/index.tsx
+++ src/components/baseComponents/Adornment/index.tsx
@@ -14,7 +14,7 @@ const InputAdornment = ({ className, type = "inherit", children }: Props) => {
   return (
     <div
       className={classNames(
-        "absolute inset-y-0 flex items-center pointer-events-none",
+        "absolute inset-y-0 flex items-center",
         className,
         {
           ["bg-secondary px-8 rounded-5"]: type === "fill",
