3945c96ed16f8e6e563f8d7de620eb65c87f46cd
Mihai <mihai@realtyview.com>
2023-08-24 18:12:53 +0300

    feat: add Close Icon

--- src/components/baseComponents/IconWrapper/Icons/index.ts
+++ src/components/baseComponents/IconWrapper/Icons/index.ts
@@ -1,14 +1,22 @@
 // icons
 import IconArrowTopBottom from "./list/IconArrowTopBottom";
+import IconClose from "./list/IconClose";
 import IconEye from "./list/IconEye";
 import IconMoney from "./list/IconMoney";
 import IconSearch from "./list/IconSearch";
 
 // ----------------------------------------------------------------------
-export type IconType = "arrow-top-bottom" | "eye" | "money" | "search";
+export type IconType =
+  | "arrow-top-bottom"
+  | "eye"
+  | "money"
+  | "search"
+  | "close";
+
 export const IconList = {
   "arrow-top-bottom": IconArrowTopBottom,
   eye: IconEye,
   money: IconMoney,
   search: IconSearch,
+  close: IconClose,
 };
---
+++ src/components/baseComponents/IconWrapper/Icons/list/IconClose.tsx
@@ -0,0 +1,21 @@
+const IconClose = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.42889 1.42853L10.857 10.8566M1.42889 10.8566L10.857 1.42853"
+        stroke="#2F2F38"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default IconClose;
