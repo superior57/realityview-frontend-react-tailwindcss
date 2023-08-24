10de0ea6b72f336e06036bae303d11fae3aabd43
Mihai <mihai@realtyview.com>
2023-08-24 18:20:34 +0300

    feat: add placeholder props and adjusted Close icon display in AutoComplete component

--- src/components/baseComponents/AutoComplete/index.tsx
+++ src/components/baseComponents/AutoComplete/index.tsx
@@ -18,6 +18,7 @@ type Props = {
   filterKey: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   value: any;
+  placeholder?: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onChange: (value: any) => void;
   onClear?: VoidFunction;
@@ -27,6 +28,7 @@ const AutoComplete = ({
   options = [],
   value,
   filterKey,
+  placeholder,
   onChange,
   onClear,
 }: Props) => {
@@ -45,75 +47,82 @@ const AutoComplete = ({
   return (
     <div className="relative">
       <Combobox value={value} onChange={onChange}>
-        <div className="relative">
-          <Combobox.Input
-            as={TextField}
-            startAdornment={<Icon name="search" />}
-            endAdornment={
-              <Button color="inherit" onClick={onClear}>
-                <Icon name="close" />
-              </Button>
-            }
-            inputClassName="aria-expanded:rounded-b-none"
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            displayValue={(option: any) => option?.[filterKey]}
-            onChange={(event) => setQuery(event.target.value)}
-          />
+        {({ open }) => (
+          <div className="relative">
+            <Combobox.Input
+              as={TextField}
+              startAdornment={<Icon name="search" />}
+              inputClassName="aria-expanded:rounded-b-none"
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              displayValue={(option: any) => option?.[filterKey]}
+              onChange={(event) => setQuery(event.target.value)}
+              placeholder={placeholder}
+              {...(open
+                ? {
+                    endAdornment: (
+                      <Button color="inherit" onClick={onClear}>
+                        <Icon name="close" />
+                      </Button>
+                    ),
+                  }
+                : null)}
+            />
 
-          <Transition
-            as={Fragment}
-            leave="transition-all ease-in duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            afterLeave={() => setQuery("")}
-          >
-            <Combobox.Options className="absolute w-full py-8 overflow-auto transition-opacity max-h-60 rounded-b-5 border-1 border-netural-dark border-t-netural">
-              {filteredOptions.length === 0 && query !== "" ? (
-                <Typography variant="body2" className="px-16">
-                  Nothing found.
-                </Typography>
-              ) : (
-                filteredOptions.map((option) => (
-                  <Combobox.Option
-                    key={option.id}
-                    className={({ active }) =>
-                      classNames(
-                        "relative flex cursor-default select-none py-8 px-16 pr-4 hover:bg-neutral-100",
-                        {
-                          ["bg-netural"]: active,
-                        }
-                      )
-                    }
-                    value={option}
-                  >
-                    <Typography
-                      variant="body2"
-                      className="whitespace-pre"
-                      color="secondary"
+            <Transition
+              as={Fragment}
+              leave="transition-all ease-in duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              afterLeave={() => setQuery("")}
+            >
+              <Combobox.Options className="absolute w-full py-8 overflow-auto transition-opacity max-h-60 rounded-b-5 border-1 border-netural-dark border-t-netural">
+                {filteredOptions.length === 0 && query !== "" ? (
+                  <Typography variant="body2" className="px-16">
+                    Nothing found.
+                  </Typography>
+                ) : (
+                  filteredOptions.map((option) => (
+                    <Combobox.Option
+                      key={option.id}
+                      className={({ active }) =>
+                        classNames(
+                          "relative flex cursor-default select-none py-8 px-16 pr-4 hover:bg-neutral-100",
+                          {
+                            ["bg-netural"]: active,
+                          }
+                        )
+                      }
+                      value={option}
                     >
-                      {splitTextByQuery(option[filterKey], query)[0]}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      className="block truncate whitespace-pre"
-                    >
-                      {splitTextByQuery(option[filterKey], query)[1]}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      className="whitespace-pre"
-                      color="secondary"
-                    >
-                      {splitTextByQuery(option[filterKey], query)[2]}
-                    </Typography>
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-        </div>
+                      <Typography
+                        variant="body2"
+                        className="whitespace-pre"
+                        color="secondary"
+                      >
+                        {splitTextByQuery(option[filterKey], query)[0]}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="block truncate whitespace-pre"
+                      >
+                        {splitTextByQuery(option[filterKey], query)[1]}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="whitespace-pre"
+                        color="secondary"
+                      >
+                        {splitTextByQuery(option[filterKey], query)[2]}
+                      </Typography>
+                    </Combobox.Option>
+                  ))
+                )}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        )}
       </Combobox>
     </div>
   );
