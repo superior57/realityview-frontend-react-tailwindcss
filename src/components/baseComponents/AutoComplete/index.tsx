6818617ab8a086246aacb387497a1f24ba9b1d64
Mihai <mihai@realtyview.com>
2023-08-24 18:11:50 +0300

    refactor: added props to auto-complete component

--- src/components/baseComponents/AutoComplete/index.tsx
+++ src/components/baseComponents/AutoComplete/index.tsx
@@ -7,27 +7,36 @@ import Icon from "../IconWrapper";
 // components
 import TextField from "../TextField";
 import Typography from "../Typography";
+import Button from "../Button";
+// utils
 import { splitTextByQuery } from "@/shared/services/utils";
 
 // ----------------------------------------------------------------------
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-];
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  options: Array<any>;
+  filterKey: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  value: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onChange: (value: any) => void;
+  onClear?: VoidFunction;
+};
 
-const AutoComplete = () => {
-  const [selected, setSelected] = useState(people[0]);
+const AutoComplete = ({
+  options = [],
+  value,
+  filterKey,
+  onChange,
+  onClear,
+}: Props) => {
   const [query, setQuery] = useState("");
 
-  const filteredPeople =
+  const filteredOptions =
     query === ""
-      ? people
-      : people.filter((person) =>
-          person.name
+      ? options
+      : options.filter((option) =>
+          option[filterKey]
             .toLowerCase()
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
@@ -35,13 +44,19 @@ const AutoComplete = () => {
 
   return (
     <div className="relative">
-      <Combobox value={selected} onChange={setSelected}>
+      <Combobox value={value} onChange={onChange}>
         <div className="relative">
           <Combobox.Input
             as={TextField}
             startAdornment={<Icon name="search" />}
+            endAdornment={
+              <Button color="inherit" onClick={onClear}>
+                <Icon name="close" />
+              </Button>
+            }
             inputClassName="aria-expanded:rounded-b-none"
-            displayValue={(person: { name: string }) => person.name}
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            displayValue={(option: any) => option?.[filterKey]}
             onChange={(event) => setQuery(event.target.value)}
           />
 
@@ -55,14 +70,14 @@ const AutoComplete = () => {
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options className="absolute w-full py-8 overflow-auto transition-opacity max-h-60 rounded-b-5 border-1 border-netural-dark border-t-netural">
-              {filteredPeople.length === 0 && query !== "" ? (
+              {filteredOptions.length === 0 && query !== "" ? (
                 <Typography variant="body2" className="px-16">
                   Nothing found.
                 </Typography>
               ) : (
-                filteredPeople.map((person) => (
+                filteredOptions.map((option) => (
                   <Combobox.Option
-                    key={person.id}
+                    key={option.id}
                     className={({ active }) =>
                       classNames(
                         "relative flex cursor-default select-none py-8 px-16 pr-4 hover:bg-neutral-100",
@@ -71,27 +86,27 @@ const AutoComplete = () => {
                         }
                       )
                     }
-                    value={person}
+                    value={option}
                   >
                     <Typography
                       variant="body2"
                       className="whitespace-pre"
                       color="secondary"
                     >
-                      {splitTextByQuery(person.name, query)[0]}
+                      {splitTextByQuery(option[filterKey], query)[0]}
                     </Typography>
                     <Typography
                       variant="body2"
                       className="block truncate whitespace-pre"
                     >
-                      {splitTextByQuery(person.name, query)[1]}
+                      {splitTextByQuery(option[filterKey], query)[1]}
                     </Typography>
                     <Typography
                       variant="body2"
                       className="whitespace-pre"
                       color="secondary"
                     >
-                      {splitTextByQuery(person.name, query)[2]}
+                      {splitTextByQuery(option[filterKey], query)[2]}
                     </Typography>
                   </Combobox.Option>
                 ))
