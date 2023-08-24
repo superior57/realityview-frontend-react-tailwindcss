9162fe618cd30c270b164bf02199e0f681ea3dbd
Mihai <mihai@realtyview.com>
2023-08-24 16:27:47 +0300

    fix: make Textfield component to be able to used for ref

--- src/components/baseComponents/TextField/index.tsx
+++ src/components/baseComponents/TextField/index.tsx
@@ -12,45 +12,50 @@ type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   endAdornmentType?: InputAdornmentType;
 };
 
-const TextField = ({
-  className,
-  inputClassName,
-  startAdornment,
-  endAdornment,
-  startAdornmentType,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  endAdornmentType,
-  ...other
-}: Props) => {
-  return (
-    <div className={classNames("relative w-full", className)}>
-      {startAdornment ? (
-        <InputAdornment className="left-0 pl-8" type={startAdornmentType}>
-          {startAdornment}
-        </InputAdornment>
-      ) : null}
+const TextField = React.forwardRef(
+  (
+    {
+      className,
+      inputClassName,
+      startAdornment,
+      endAdornment,
+      startAdornmentType,
+      endAdornmentType,
+      ...other
+    }: Props,
+    ref: React.ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <div className={classNames("relative w-full", className)}>
+        {startAdornment ? (
+          <InputAdornment className="left-0 pl-8" type={startAdornmentType}>
+            {startAdornment}
+          </InputAdornment>
+        ) : null}
 
-      <input
-        type="text"
-        className={classNames(
-          inputClassName,
-          "body1 bg-netural rounded-5 h-40 text-primary p-16 outline-none w-full placeholder:caption1 placeholder:tracking-normal",
-          {
-            ["pl-40"]: !!startAdornment,
-            ["pr-40"]: !!endAdornment,
-            ["font-bold tracking-2"]: other.type === "password",
-          }
-        )}
-        {...other}
-      />
+        <input
+          type="text"
+          className={classNames(
+            inputClassName,
+            "body1 bg-netural rounded-5 h-40 text-primary p-16 outline-none w-full placeholder:caption1 placeholder:tracking-normal border-1 border-transparent focus:border-netural-dark",
+            {
+              ["pl-40"]: !!startAdornment,
+              ["pr-40"]: !!endAdornment,
+              ["font-bold tracking-2"]: other.type === "password",
+            }
+          )}
+          ref={ref}
+          {...other}
+        />
 
-      {endAdornment ? (
-        <InputAdornment className="right-0 pr-8" type={endAdornmentType}>
-          {endAdornment}
-        </InputAdornment>
-      ) : null}
-    </div>
-  );
-};
+        {endAdornment ? (
+          <InputAdornment className="right-0 pr-8" type={endAdornmentType}>
+            {endAdornment}
+          </InputAdornment>
+        ) : null}
+      </div>
+    );
+  }
+);
 
 export default TextField;
