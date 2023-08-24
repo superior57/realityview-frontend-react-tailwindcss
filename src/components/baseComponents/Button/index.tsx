265fe99126069914617b78e4630df5cee1a87207
Mihai <mihai@realtyview.com>
2023-08-24 18:12:33 +0300

    feat: add props to Button component

--- src/components/baseComponents/Button/index.tsx
+++ src/components/baseComponents/Button/index.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import classNames from "classnames";
 
 // ----------------------------------------------------------------------
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  color?: "inherit" | "primary" | "secondary";
+  variant?: "contained" | "outlined" | "text";
+};
 
-const Button = ({ className, children, ...other }: Props) => {
+const Button = ({
+  className,
+  color = "primary",
+  variant = "contained",
+  children,
+  ...other
+}: Props) => {
   return (
     <button
       className={classNames(
-        "bg-button-primary text-white w-215 h-40 rounded-5 font-semibold leading-23 text-18 font-montserrat hover:bg-button-primary-hover disabled:bg-button-disabled",
+        "w-215 h-40 rounded-5 font-semibold leading-23 text-18 font-montserrat disabled:bg-button-disabled cursor-pointer",
+        {
+          ["bg-button-primary hover:bg-button-primary-hover text-white"]:
+            color === "primary" && variant === "contained",
+          ["border-1 border-primary text-primary"]:
+            color === "primary" && variant === "outlined",
+        },
         className
       )}
       {...other}
