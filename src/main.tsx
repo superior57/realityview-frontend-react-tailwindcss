a0f067d889facfdcb6f2e6757ab52b72ec0fa8d0
Mihai <mihai@realtyview.com>
2023-08-25 11:48:24 +0300

    fix: lint issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  RouterProvider,
-} from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store } from '@/redux/store'
-import { router } from './pages/Router'
-import '@/assets/scss/index.scss'
-import Bugsnag from '@bugsnag/js'
-import BugsnagPluginReact from '@bugsnag/plugin-react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store } from '@/redux/store';
+import { router } from './pages/Router';
+import '@/assets/scss/index.scss';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginReact from '@bugsnag/plugin-react';
+
+// datepicker style
+import 'react-datepicker/dist/react-datepicker.css';
+
+// ----------------------------------------------------------------------
 
 Bugsnag.start({
   apiKey: import.meta.env.VITE_BUGSANG_API_KEY,
-  plugins: [new BugsnagPluginReact()]
-})
-const ErrorBoundary = (Bugsnag as any).getPlugin("react").createErrorBoundary(React)
+  plugins: [new BugsnagPluginReact()],
+});
+// eslint-disable-next-line react-refresh/only-export-components, @typescript-eslint/no-explicit-any
+const ErrorBoundary = (Bugsnag as any)
+  .getPlugin('react')
+  .createErrorBoundary(React);
 
-let persistor = persistStore(store)
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
@@ -27,5 +33,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router} />
       </PersistGate>
     </Provider>
-  </ErrorBoundary>,
-)
+  </ErrorBoundary>
+);
--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -1,55 +1,57 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import type { PayloadAction, ThunkAction, AnyAction } from '@reduxjs/toolkit'
-import type { RootState } from '../store'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import type { PayloadAction, ThunkAction, AnyAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store';
+import axios from 'axios';
 
 // Define a type for the slice state
 interface UserState {
-  isSignin:boolean,
+  isSignin: boolean;
 }
 
 // Define the initial state using that type
 const initialState: UserState = {
-  isSignin:false,
-}
+  isSignin: false,
+};
 
 export const userSlice = createSlice({
   name: 'users',
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    setSignin:(state, action:PayloadAction<boolean>)=>{
-      state.isSignin = action.payload
+    setSignin: (state, action: PayloadAction<boolean>) => {
+      state.isSignin = action.payload;
     },
-
   },
-  extraReducers:(builder) => {
-    builder.addCase(signIn.fulfilled, (state, action)=>{
-      state.isSignin = action.payload
-    })
-  }
-})
+  extraReducers: (builder) => {
+    builder.addCase(signIn.fulfilled, (state, action) => {
+      state.isSignin = action.payload;
+    });
+  },
+});
 
-export const usersAction = userSlice.actions
+export const usersAction = userSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const getIsSignin = (state: RootState) => state.user.isSignin
+export const getIsSignin = (state: RootState) => state.user.isSignin;
 
-export default userSlice.reducer
+export default userSlice.reducer;
 
 export const signIn = createAsyncThunk(
   'user/signin',
-  async (data:any) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async (data: any) => {
     const response = await axios.post('user/signin', {
-      email:data.email,
-      password:data.password,
-    })
-    return response.data
+      email: data.email,
+      password: data.password,
+    });
+    return response.data;
   }
-)
+);
 
-export const setSignin =(value:boolean):ThunkAction<void, RootState, unknown, AnyAction>=>{
-  return async(dispatch)=>{
-      await dispatch(usersAction.setSignin(value))
-  }
-}
+export const setSignin = (
+  value: boolean
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return async (dispatch) => {
+    await dispatch(usersAction.setSignin(value));
+  };
+};
