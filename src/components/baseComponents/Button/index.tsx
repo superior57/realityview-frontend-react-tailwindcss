1858a9033c563d1636428e5d7a7b6980188acc29
Mihai <mihai@realtyview.com>
2023-08-25 11:47:54 +0300

    refactor: add size props to Button component

--- src/components/baseComponents/Button/index.tsx
+++ src/components/baseComponents/Button/index.tsx
@@ -5,24 +5,28 @@ import classNames from 'classnames';
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   color?: 'inherit' | 'primary' | 'secondary';
   variant?: 'contained' | 'outlined' | 'text';
+  size?: 'medium' | 'small';
 };
 
 const Button = ({
   className,
   color = 'primary',
   variant = 'contained',
+  size = 'medium',
   children,
   ...other
 }: Props) => {
   return (
     <button
       className={classNames(
-        'w-215 h-40 rounded-5 font-semibold leading-23 text-18 font-montserrat disabled:bg-button-disabled cursor-pointer',
+        'w-215 rounded-5 font-semibold leading-23 text-18 font-montserrat disabled:bg-button-disabled cursor-pointer',
         {
           ['bg-button-primary hover:bg-button-primary-hover text-white']:
             color === 'primary' && variant === 'contained',
           ['border-1 border-primary text-primary']:
             color === 'primary' && variant === 'outlined',
+          ['h-40']: size === 'medium',
+          ['h-21']: size === 'small',
         },
         className
       )}
